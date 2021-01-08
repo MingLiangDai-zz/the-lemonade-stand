6919b9d9a89b04ba2ed87f017ede4766ea3c821a
Ming Liang Dai <ming@JLs-MacBook-Pro.local>
2021-01-08 12:10:53 -0500

    changed header

---
+++ precache-manifest.5b604e4d8952704fa7ccfad0970a4595.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb75b2237fae05c4f047588a199aab6",
+    "url": "/the-lemonade-stand/index.html"
+  },
+  {
+    "revision": "505196e6f19334e7b896",
+    "url": "/the-lemonade-stand/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "a167b40a059034b58e03",
+    "url": "/the-lemonade-stand/static/css/main.5e9a8a27.chunk.css"
+  },
+  {
+    "revision": "505196e6f19334e7b896",
+    "url": "/the-lemonade-stand/static/js/2.03493dc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the-lemonade-stand/static/js/2.03493dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a167b40a059034b58e03",
+    "url": "/the-lemonade-stand/static/js/main.c74ad20c.chunk.js"
+  },
+  {
+    "revision": "f97ef677300413f53636",
+    "url": "/the-lemonade-stand/static/js/runtime-main.8be1f750.js"
+  },
+  {
+    "revision": "f8dcdab2d910ce4db7c22d3433ade97a",
+    "url": "/the-lemonade-stand/static/media/hero.f8dcdab2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cef58151e51950431d12584a8ea1a122.js"
+  "/the-lemonade-stand/precache-manifest.5b604e4d8952704fa7ccfad0970a4595.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-lemonade-stand/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
