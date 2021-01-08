7c73890d5098cd58eb2f2513be4509b9b9f6ecde
Ming Liang Dai <ming@JLs-MacBook-Pro.local>
2021-01-08 11:34:59 -0500

    added build

---
+++ precache-manifest.8db61d32858b22a90b5184dc8ed76444.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae11fab5ecdbcef4b4c37e3bd3c7cffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505196e6f19334e7b896",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "b778efc3ffb1e5e4dea5",
+    "url": "/static/css/main.5e9a8a27.chunk.css"
+  },
+  {
+    "revision": "505196e6f19334e7b896",
+    "url": "/static/js/2.03493dc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.03493dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b778efc3ffb1e5e4dea5",
+    "url": "/static/js/main.e0887c8f.chunk.js"
+  },
+  {
+    "revision": "19db88148691745c6ebc",
+    "url": "/static/js/runtime-main.8138c08a.js"
+  },
+  {
+    "revision": "f8dcdab2d910ce4db7c22d3433ade97a",
+    "url": "/static/media/hero.f8dcdab2.svg"
+  }
+]);
